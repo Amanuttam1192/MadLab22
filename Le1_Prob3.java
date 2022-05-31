6ac52dc85ecafeeb98a81253a3a54600db3b5f62
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-05-31 20:18:23 +0530

    Create Le1_Prob3.java

---
+++ Le1_Prob3.java
@@ -0,0 +1,8 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a = Integer.parseInt(args[0]);
+	    for(int i = 1; i <= 10; i++)
+		    System.out.println(a + " X " + i + " = " + a * i);
+	}
+}
