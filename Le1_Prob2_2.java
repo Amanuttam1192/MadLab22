3f8cf9eef280fe3ba75f7666f64d828d5534e99e
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-05-31 20:15:18 +0530

    Create Le1_Prob2_2.java

---
+++ Le1_Prob2_2.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main (String args[]){
+    int a = Integer.parseInt(args[0]);
+    for (int i = a; i >= 0; i--) 
+    {
+        for (int j = 0; j < a - i; j++) 
+        {
+            System.out.print(" ");
+        }
+        for (int k = 0; k <= i; k++) 
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    
+    }
+}
