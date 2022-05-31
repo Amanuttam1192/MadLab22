ecb87471dbf89b493e923c3beeecfad9b7fd1d3b
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-05-31 20:57:32 +0530

    Create Le2_q1_main.java

---
+++ Le2_q1_main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main (String args[]){
+    Mother m = new Mother();
+    m.show(); // show of Mother is called
+    Child ch = new Child();
+    ch.show();
+    }
+}
