c513fe7757a7ef449fc48378d358d411be6fa243
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-05-31 21:01:07 +0530

    Update Le2_q2_main.java

--- Le2_q2_main.java
+++ Le2_q2_main.java
@@ -1,1 +1,9 @@
-
+public class Main
+{
+    public static void main (String args[]){
+    Mother m = new Mother();
+    m.show();
+    Child ch = new Child();
+    ch.show(); // the show function of Child class over rides the show function inherited from Mother and called Overriding function and Mother show is called Overridden function
+    }
+}
